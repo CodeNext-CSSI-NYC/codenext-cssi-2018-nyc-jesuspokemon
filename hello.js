85cc6e4942707266afcd0382f97a840af48828fa
Jesus Barrera <JeSuSrrera@gmail.com>
2018-07-10 12:57:40 -0400

    just trying to make a input

--- hello.js
+++ hello.js
@@ -1,6 +1,15 @@
 // var readline = require("readline-sync");
 // var username = readline.question("Please enter your name: ");
 // console.log( "Hello " + username + "!");
+
+// var readline = require("readline-sync");
+// var username = readline.question("enter the number 144: ");
+// console.log( "the square root of 144 is 12" + "!");
+
+// var readline = require("readline-sync");
+// var username = readline.question("Please enter your name: ");
+// console.log( username + " hello your name has 5 letters" + "!");
 var readline = require("readline-sync");
-var username = readline.question("enter the number 144: ");
-console.log( "the square root of 144 is 12" + "!");
+var username = readline.question("Please enter any word: ");
+var answer =
+console.log( "Hello the word " + username + " starts with the letter " + answer);
