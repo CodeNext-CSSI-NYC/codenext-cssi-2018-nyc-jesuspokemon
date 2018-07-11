45a1967e6c92b20600ea077e8576b18ce0e23e96
Jesus Barrera <JeSuSrrera@gmail.com>
2018-07-11 12:57:12 -0400

    worked on code

---
+++ test-conditionals.js
@@ -0,0 +1,73 @@
+// Complete these one at a time. Test each one several times to make sure it works.
+// When you are done with each one, comment it out when you move to the next one, so it doesn't keep showing up.
+// 1. Ask the user for a number and print whether it is positive or negative or zero.
+
+
+// let readline = require("readline-sync");
+//
+// let number = parseFloat(readline.question("enter any number "));
+//
+// if (number == 0) {
+//   console.log("This number is zero: ");
+// } else if (number > 0) {
+//   console.log("This number is a positive number: ");
+// } else if (number < 0) {
+//   console.log("This number is a negative number: ");
+// }
+
+
+// 2. Ask the user to enter three numbers, and then print out the biggest number they entered.
+
+
+// let readline = require("readline-sync");
+//
+// let num1 = parseFloat(readline.question("enter the first number: "));
+// let num2 = parseFloat(readline.question("enter the second number: "));
+// let num3 = parseFloat(readline.question("enter the third number: "));
+//
+// if (num1 > num2 && num1 > num3) {
+//   console.log("The first one was biggest");
+// } else if (num1 < num2 && num2 > num3) {
+//   console.log("The second one was biggest");
+// }else {
+//   console.log("The third one was biggest");
+// }
+
+
+// 3. Ask the user for a number and print whether it is odd or even
+
+
+// let readline = require("readline-sync");
+//
+// let num = parseFloat(readline.question("enter any number: "));
+//
+// if ((num % 2) == 0) {
+//   console.log("This number is an positive number: ");
+// } else if ((num % 2) == 1) {
+//   console.log("This number is a odd number: ");
+// }
+
+
+// 4. Ask the user to enter a letter and tell them if it is a vowel or consonant
+
+
+let readline = require("readline-sync");
+
+let Letter = parseFloat(readline.question("Please enter any number: "));
+
+if (Letter == a || e || i || o || u) {
+  console.log("This number is a vowel: ");
+} else {
+  console.log("This number is a odd number: ");
+}
+
+
+// 5. Ask the user to enter a year, and tell them if it was a leap year.
+// (You can use Google if you're not sure when leap years happen.)
+
+// 6. Ask the user if they have been a citizen for at least 9 years (yes or no), ask them their age, and ask if they live in New York state.
+// If they answer yes, their age is above 30, and yes, then tell them they can run for senate in NY. Otherwise, say that they cannot.
+
+// 7. Write a program that asks the user to enter a whole number 1-7 and prints out the corresponding day of the week.
+// If you want another challenge, look up how to use switch statements and use a switch instead of if-else for this.
+// Note that we usually don't bother with switch statements unless there are more than a few elses.
