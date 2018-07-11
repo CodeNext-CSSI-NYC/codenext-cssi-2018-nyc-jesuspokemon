d7d55f5f4505e1ab51b177c16c1066520b6ee81b
Jesus Barrera <JeSuSrrera@gmail.com>
2018-07-11 10:40:32 -0400

    done with practice

--- hello.js
+++ hello.js
@@ -11,12 +11,24 @@
 // console.log( username + " hello your name has 5 letters" + "!");
 
 // var readline = require("readline-sync");
-// var username = readline.question("Please enter any word: ");
+// var username = readline.question("Please enter your name: ");
 // name = username
 // var answer = username.charAt(0)
-// console.log( "Hello the word " + username + " starts with the letter " + answer);
+// console.log( "Hello " + username + " your name starts with the letter " + answer);
+
+// version 2
+
+// var readline = require("readline-sync");
+// var username = readline.question("Please enter your name: ");
+// name = username
+// var answer = username.charAt(0)
+// console.log( "Hello " + username + " your name starts with the letter " + answer);
+
 var readline = require("readline-sync");
-var username = readline.question("Please enter your name: ");
-name = username
-var answer = username.charAt(0)
-console.log( "Hello " + username + " your name starts with the letter " + answer);
+console.log(" Ax^2 + Bx + C")
+var a = readline.question("Please enter the A value")
+var b = readline.question("please enter the B value")
+var c = readline.question("please enter the C value")
+console.log( a + "x^2" + " + " + b + "x" + " + " + c)
+
+var answer = readline.equation("Please enter X value")
