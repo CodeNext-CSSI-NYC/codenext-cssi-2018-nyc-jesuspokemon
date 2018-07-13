8e4f8d5e603f3dded5a597eabbc2f837b184ad38
Jesus Barrera <JeSuSrrera@gmail.com>
2018-07-13 12:24:30 -0400

    working on questions

--- conditionals-practice.js
+++ conditionals-practice.js
@@ -10,13 +10,13 @@
 // }
 
 
-// if(x == 2) {
-//   console.log("x is 2");
-// } else if(x == 3) {
-//   console.log("x is 3");
-// } else {
-//   console.log("x is neither 2 nor 3");
-// }
+if(x == 2) {
+  console.log("x is 2");
+} else if(x == 3) {
+  console.log("x is 3");
+} else {
+  console.log("x is neither 2 nor 3");
+}
 
 
 // const readline = require("readline-sync");
